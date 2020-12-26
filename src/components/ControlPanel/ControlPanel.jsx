bf598a92eda56fa4bed7c5e967fc7429e154d843
Brent Ahrens <cidida@gmail.com>
2020-12-25 23:08:05 -0800

    adds basic screen

--- src/components/ControlPanel/ControlPanel.jsx
+++ src/components/ControlPanel/ControlPanel.jsx
@@ -3,11 +3,13 @@ import "../../sass/_control-panel.scss"
 
 //components
 import ControlBtns from "../ControlBtns/ControlBtns"
+import ControlScreen from "../ControlScreen/ControlScreen"
 
 const ControlPanel = () => {
     return (
         <div className="control-panel">
             <ControlBtns />
+            <ControlScreen />
         </div>
     )
 }
---
+++ src/components/ControlScreen/ControlScreen.jsx
@@ -0,0 +1,14 @@
+import ControlBtns from "../ControlBtns/ControlBtns"
+
+//scss
+import "../../sass/_control-screen.scss"
+
+const ControlScreen = () => {
+    return (
+        <div className="control-screen">
+
+        </div>
+    )
+}
+
+export default ControlScreen
